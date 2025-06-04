7b88bb053ebaaabc3a597e1a515b29217992eaf3
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-06-04 10:13:48 +0530

    set the header and app

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "./Button";
 
 const Header = () => {
   const [activeMenu, setActiveMenu] = useState("Home");

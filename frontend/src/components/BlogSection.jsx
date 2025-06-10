0d4201d92e3eddb50d0f26441925cdb4242b4cd8
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-06-10 17:17:25 +0530

    height full of blogs

--- frontend/src/components/BlogSection.jsx
+++ frontend/src/components/BlogSection.jsx
@@ -309,7 +309,7 @@ const BlogSection = () => {
                       <img
                         src={post.image}
                         alt={post.title}
-                        className="w-full h-64 object-cover group-hover:scale-105 transition-transform duration-500"
+                        className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
                       />
                       <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
                     </div>
